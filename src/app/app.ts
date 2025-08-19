cff12cbfd8c5df410e22feacfaca3733b3065d43
Isaac Hayfron <isaachayfron24@gmail.com>
2025-08-19 14:00:32 +0000

    chore: migrate testing framework to Jest, update Angular configuration, and enhance app component tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'Portfolio';
+
+  @Input() count = 0;
 }
