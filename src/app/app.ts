8963a26c9587c88c4fcb79d6cf5edcaa2e356cb5
Isaac Hayfron <isaachayfron24@gmail.com>
2025-08-20 08:21:42 +0000

    feat: integrate header component into app layout and remove placeholder content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
