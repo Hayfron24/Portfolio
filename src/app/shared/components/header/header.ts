f5600a892e0d46f99d32c69b84a7493328739879
Isaac Hayfron <isaachayfron24@gmail.com>
2025-08-29 10:21:42 +0000

    feat: implement conditional header display with slide-in animation and refactor SCSS styles

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from "@angular/animations";
+import { Component, OnInit, signal } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   imports: [],
-  templateUrl: './header.html',
-  styleUrl: './header.scss'
+  animations: [
+    trigger("slideIn", [
+      transition(":enter", [
+        style({ transform: "translateY(-100%)", opacity: 0 }),
+        animate(
+          "1s ease-in",
+          style({ transform: "translateY(0)", opacity: 1 })
+        ),
+      ]),
+    ]),
+  ],
+  templateUrl: "./header.html",
+  styleUrl: "./header.scss",
 })
-export class Header {
+export class Header implements OnInit {
+  public displayHeader = signal(false);
 
+  public ngOnInit(): void {
+    setTimeout(() => {
+      this.displayHeader.set(true);
+    }, 1000);
+  }
 }
