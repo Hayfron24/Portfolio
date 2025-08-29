4ea934b5ec36693e6c7ef826d280444178a7a2cb
Isaac Hayfron <isaachayfron24@gmail.com>
2025-08-29 10:21:02 +0000

    feat: enhance app configuration and layout by adding Home component and async animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,13 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideAnimationsAsync()
   ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
+import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, Home],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
